a3ef467893e4bd18a2bc938a72a78572560a6b9c
rajen <2019saurabh.borawake@ves.ac.in>
2022-06-06 09:09:49 +0530

    models and some of routes created!

--- mern-exercise-tracker/backend/server.js
+++ mern-exercise-tracker/backend/server.js
@@ -7,22 +7,19 @@ const port=process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 const uri=process.env.ATLAS_URI;
-// mongoose.connect(uri,{useNewUrlParser:true,useCreateIndex:true});
-mongoose.connect(
-    uri,
-    {
-      
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    },
-    (err) =>
-      err ? console.log(err) : console.log(
-        "Connected to yourDB-name database")
-  );
+mongoose.connect(uri, {useNewUrlParser: true});
+
 const connection=mongoose.connection;
 connection.once('open',()=>{
     console.log("MongoDB Database connection established successfully!");
 })
+
+const exerciseRouter=require('./routes/exercises');
+const usersRouter=require('./routes/users');
+
+app.use('/exercises',exerciseRouter);
+app.use('/users',usersRouter);
+
 app.listen(port,()=>{
     console.log(`server is running on port ${port}`);
 })
